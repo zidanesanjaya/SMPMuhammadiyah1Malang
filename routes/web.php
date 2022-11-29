91eb9d348ae058ec6624803a97252943a659d815
Zidane071 <zidanesanjaya81@Gmail.com>
2022-11-29 13:37:23 +0700

    z

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/cermus', function () {
+    return view('cermus');
+});
+
+Route::get('/galeri', function () {
+    return view('galeri');
+});
+
+Route::get('/ppdb', function () {
+    return view('ppdb');
+});
+
+Route::get('/aksi', function () {
+    return view('aksi');
+});
+
