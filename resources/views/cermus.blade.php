30f4c9c1a8ad09c82b327a15fd5daab7252090f1
Zidane071 <zidanesanjaya81@Gmail.com>
2022-12-01 14:13:33 +0700

    z

--- resources/views/cermus.blade.php
+++ resources/views/cermus.blade.php
@@ -50,23 +50,16 @@
   </header><!-- End Header -->
 
   <main id="main" data-aos="fade-in">
-    <!-- ======= Breadcrumbs ======= -->
-    <div class="breadcrumbs">
-      <div class="container">
-        <h2>Courses</h2>
-        <p>Est dolorum ut non facere possimus quibusdam eligendi voluptatem. Quia id aut similique quia voluptas sit quaerat debitis. Rerum omnis ipsam aperiam consequatur laboriosam nemo harum praesentium. </p>
-      </div>
-    </div>
-    <!-- End Breadcrumbs -->
-
+    <br><br>
     <!-- ======= Popular Courses Section ======= -->
     <section id="cermus" class="courses">
     <div class="container" data-aos="fade-up">
-
         <div class="section-title">
-            <h2>CERMUS</h2>
             <p>Cerita Muhasa</p>
+            <h2></h2>
         </div>
+
+    <br>
 
         <div class="row" data-aos="zoom-in" data-aos-delay="100">
 
@@ -151,16 +144,94 @@
             </div>
         </div>
         <!-- End Course Item-->
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="course-item">
+            <img src="/img/course-1.jpg" class="img-fluid" alt="...">
+            <div class="course-content">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                <h4>Web Development</h4>
+                <p class="price">$169</p>
+                </div>
+
+                <h3><a href="course-details.html">Website Design</a></h3>
+                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+                <div class="trainer d-flex justify-content-between align-items-center">
+                <div class="trainer-profile d-flex align-items-center">
+                    <img src="/img/user.png" class="img-fluid" alt="">
+                    <span>Antonio</span>
+                </div>
+                <div class="trainer-rank d-flex align-items-center">
+                    <i class="bx bx-user"></i>&nbsp;50
+                    &nbsp;&nbsp;
+                    <i class="bx bx-heart"></i>&nbsp;65
+                </div>
+                </div>
+            </div>
+            </div>
+        </div> <!-- End Course Item-->
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="course-item">
+            <img src="/img/course-2.jpg" class="img-fluid" alt="...">
+            <div class="course-content">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                <h4>Marketing</h4>
+                <p class="price">$250</p>
+                </div>
+
+                <h3><a href="course-details.html">Search Engine Optimization</a></h3>
+                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+                <div class="trainer d-flex justify-content-between align-items-center">
+                <div class="trainer-profile d-flex align-items-center">
+                    <img src="/img/user.png" class="img-fluid" alt="">
+                    <span>Lana</span>
+                </div>
+                <div class="trainer-rank d-flex align-items-center">
+                    <i class="bx bx-user"></i>&nbsp;35
+                    &nbsp;&nbsp;
+                    <i class="bx bx-heart"></i>&nbsp;42
+                </div>
+                </div>
+            </div>
+            </div>
+        </div> <!-- End Course Item-->
+
         
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="course-item">
+            <img src="/img/course-3.jpg" class="img-fluid" alt="...">
+            <div class="course-content">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h4>Content</h4>
+                    <p class="price">$180</p>
+                </div>
+
+                <h3><a href="course-details.html">Copywriting</a></h3>
+                    <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+                <div class="trainer d-flex justify-content-between align-items-center">
+                    <div class="trainer-profile d-flex align-items-center">
+                        <img src="/img/user.png" class="img-fluid" alt="">
+                        <span>Brandon</span>
+                    </div>
+
+                <div class="trainer-rank d-flex align-items-center">
+                    <i class="bx bx-user"></i>&nbsp;20
+                    &nbsp;&nbsp;
+                    <i class="bx bx-heart"></i>&nbsp;85
+                </div>
+
+                </div>
+            </div>
+            </div>
         </div>
-        <!-- <div class="mt-3 d-flex flex-row-reverse p-2 bd-highlight"> -->
-        <div class="mt-3 d-flex flex-row-reverse p-2 bd-highlight">
-                <a href="{{ url ('http://127.0.0.1:8000/cermus') }}" class="get-started-btn">Berita Lainnya</a>
+        <!-- End Course Item-->
+
         </div>
-        <!-- </div> -->
     </div>
     </section>
     <!-- End Popular Courses Section -->
+
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
