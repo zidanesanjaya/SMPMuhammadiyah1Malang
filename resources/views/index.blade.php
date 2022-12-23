de31322e4d89cf6d49a5505f761f3adf234bee33
Zidane071 <zidanesanjaya81@Gmail.com>
2022-12-23 15:18:31 +0700

    z

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -86,13 +86,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" style="position: absolute; z-index: 1;" data-interval="100">
     <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="2500">
-            <img src="/img/indexCarousel1.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%);">
+            <img src="/img/indexCarousel1.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%); size: 100px;">
         </div>
         <div class="carousel-item" data-bs-interval="2500">
-            <img src="/img/indexCarousel2.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%);">
+            <img src="/img/indexCarousel2.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%); size: 100px;">
         </div>
         <div class="carousel-item" data-bs-interval="2500" repeat>
-            <img src="/img/indexCarousel3.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%);">
+            <img src="/img/indexCarousel3.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%); size: 100px;">
         </div>
     </div>
 </div>
